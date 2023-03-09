27c5f1dde972989b124fe766d7c8febb479c08d8
CorentinJGosling <you@example.com>
2023-03-09 22:10:27 +0100

    update design2

--- js/script.js
+++ js/script.js
@@ -7,22 +7,40 @@ const svgElementT = document.getElementById("top");
 const svgElementB = document.getElementById("bottom");
 const svgElementM = document.getElementById("middle");
 const profile = body.querySelector(".img_profile");
+
 svgElement.addEventListener("click", function () {
-  svgElement.classList.toggle("open");
-  svgElement.classList.toggle("inc_width");
-  sidebar.classList.toggle("activate");
-  profile.classList.toggle("leftMarg");
   svgElementT.classList.toggle("colorMob");
   svgElementB.classList.toggle("colorMob");
   svgElementM.classList.toggle("colorMob");
-  if (sidebar.classList.contains("activate")) {
-    sidebar.classList.remove("close");
-    sidebar.classList.add("fullwidth");
+  svgElement.classList.toggle("open");
+  sidebar.classList.toggle("fullwidth");
+  svgElement.classList.toggle("inc_width");
+  sidebar.classList.toggle("activate");
+  profile.classList.toggle("leftMarg");
+});
+// if (sidebar.classList.contains("activate")) {
+//   sidebar.classList.remove("close");
+//   sidebar.classList.add("fullwidth");
+// } else {
+//   sidebar.classList.remove("fullwidth");
+//   sidebar.classList.add("close");
+// }
+
+window.onclick = function (e) {
+  if (e.target == sidebar || e.target == svgElement) {
+    return;
   } else {
     sidebar.classList.remove("fullwidth");
     sidebar.classList.add("close");
+    svgElement.classList.remove("inc_width");
+    profile.classList.remove("leftMarg");
+    svgElement.classList.remove("open");
+    sidebar.classList.remove("activate");
+    svgElementT.classList.remove("colorMob");
+    svgElementB.classList.remove("colorMob");
+    svgElementM.classList.remove("colorMob");
   }
-});
+};
 
 var counterElement = document.getElementById("counter");
 
